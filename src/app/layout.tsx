66e146f17eb71be43972bf902514f399a1d8b4fb
aman-a-dev <amanuelantenha@gmail.com>
2026-09-08 15:21:20 +0300

    add to script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 
 export default function MainLayout({ children }: LayoutProps<"/">) {
-  return <div className="mb-24 mx-2">{children}</div>;
+  return (
+    <div className="mb-24 mx-2">
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js" />
+      </head>
+      {children}
+    </div>
+  );
 }
--- src/context/telegram-context.tsx
+++ src/context/telegram-context.tsx
@@ -1,4 +1,3 @@
-// context/telegram-context.tsx
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
