648f26661e86722b59dbabe1b64f8f45bb7ec383
aman-a-dev <amanuelantenha@gmail.com>
2026-09-08 14:59:00 +0300

    remove auth from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import { FavoritesProvider } from "@/context/favourites-context";
 import { TelegramProvider } from "@/context/telegram-context";
-import { TelegramAuth } from "@/components/primitives/telegram-auth";
 import "./globals.css";
 
 export default function MainLayout({ children }: LayoutProps<"/">) {
   return (
     <div className="mb-24 mx-2">
       <TelegramProvider>
-        <FavoritesProvider>
-          {children}
-          <TelegramAuth />
-        </FavoritesProvider>
+        <FavoritesProvider>{children}</FavoritesProvider>
       </TelegramProvider>
     </div>
   );
